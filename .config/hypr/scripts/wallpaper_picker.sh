72a96ef48adf45fa0f540a79520eed8610b6a9b3
andresparra1980 <github@andresparra.co>
2024-01-11 20:56:38 -0500

    Multiple changes for use on thinkpad and upgrades

---
+++ .config/hypr/scripts/wallpaper_picker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Declare variables
+WalPath=~/.config/hypr/wallpapers/
+
+# lauch Rofi dmenu
+cd $WalPath
+WALLPAPER=$(ls *{jpg,png} | rofi -dmenu -p "Select a Wallpaper")
+if [[ -z $WALLPAPER ]] # validate that wallpaper was selected
+then
+	exit
+fi
+
+# run pywal
+wal -i $WALLPAPER >> /dev/null
+
+# restart waybar with new colors 
+killall waybar && waybar >> /dev/null & disown 
+
+# repopulate and re-run Hyprpaper
+sed -i '1 c \preload = '"$WALLPAPER"'' ~/.config/hypr/hyprpaper.conf
+sed -i '2 c \wallpaper = eDP-1, '"$WALLPAPER"'' ~/.config/hypr/hyprpaper.conf
+killall hyprpaper && hyprpaper >> /dev/null & disown
+
